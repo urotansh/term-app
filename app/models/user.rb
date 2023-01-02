06ab695234efb652326293dc24091e485faf814d
urotansh <114655455+urotansh@users.noreply.github.com>
2023-01-02 11:34:05 +0900

    [Add] フォロー機能 User/Relationshipモデルへアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,18 @@ class User < ApplicationRecord
   has_many :note_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_notes, through: :favorites, source: :note
-
+  
+  # フォロー機能
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  # ユーザー名のバリデーション
   validates :name,
     uniqueness: true,
     length: { minimum: 1, maximum: 20 },
