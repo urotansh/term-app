d5a540acc68bb623e21ce7a1fb50d29575053778
urotansh <114655455+urotansh@users.noreply.github.com>
2023-01-16 00:06:06 +0900

    [Update] 投稿機能 textareaのEasyMDE化をajax:complete -> *.js.erb実行へ変更

--- app/javascript/packs/easymde.js
+++ app/javascript/packs/easymde.js
@@ -3,56 +3,62 @@ import EasyMDE from 'easymde';
 import 'highlightjs/styles/monokai.css';
 import hljs from 'highlightjs';
 
-// 新規投稿ページ/投稿詳細ページのテキストエリアをマークダウンエディタへ変更
-'ajax:complete turbolinks:load'.split(' ').forEach((eventName)=>{
-  document.addEventListener(eventName, (e)=>{
-    if (document.getElementById("comment_post_flg") != null){
-    return;
-    }
-    if (document.getElementById("editor") != null){
+const initMDE = function() {
+  if (document.getElementById("comment_post_flg") != null){
+  return;
+  }
+  if (document.getElementById("editor") != null){
+  // textareaをMarkdownエディタにする
+  const easymde = new EasyMDE({
+    // showIcons: ["code", "table"],
+    showIcons: ["code", "undo", "redo"],
+    shortcuts: {
+        "drawTable": "Cmd-Alt-T", // Markdownテーブルショートカット
+    },
+    renderingConfig: {
+        codeSyntaxHighlighting: true,
+        hljs: hljs
+    },
+    sideBySideFullscreen: false,
+    spellChecker: false,   // 日本語はスペルチェックに引っかかるのでスペルチェックをオフ
+    // autosave: {            // 自動保存
+    //     enabled: true,     // 自動保存有効化
+    //     delay: 1000,       // 1秒ごと保存
+    //     uniqueId: 'mde-autosave-demo' // ローカルストレージのキーに使用
+    // },
+    element: document.getElementById("editor"),
+  });
+  easymde.toggleSideBySide();
+  }
+  if (document.getElementById("editor-preview") != null){
     // textareaをMarkdownエディタにする
-    const easymde = new EasyMDE({
-      // showIcons: ["code", "table"],
-      showIcons: ["code", "undo", "redo"],
-      shortcuts: {
-          "drawTable": "Cmd-Alt-T", // Markdownテーブルショートカット
-      },
+    const easymdePreview = new EasyMDE({
+      showIcons: [],
+      autoDownloadFontAwesome: false,
+      toolbar: [{
+        name: "fullscreen",
+        action: EasyMDE.toggleFullScreen,
+        className: "fa fa-arrows-alt no-disable no-mobile",
+        title: "Toggle Fullscreen",
+      }],
+      status: false,
+      syncSideBySidePreviewScroll: false,
+      scrollbarStyle: null,
       renderingConfig: {
           codeSyntaxHighlighting: true,
           hljs: hljs
       },
-      sideBySideFullscreen: false,
-      spellChecker: false,   // 日本語はスペルチェックに引っかかるのでスペルチェックをオフ
-      // autosave: {            // 自動保存
-      //     enabled: true,     // 自動保存有効化
-      //     delay: 1000,       // 1秒ごと保存
-      //     uniqueId: 'mde-autosave-demo' // ローカルストレージのキーに使用
-      // },
-      element: document.getElementById("editor"),
+      element: document.getElementById("editor-preview"),
     });
-    easymde.toggleSideBySide();
-    }
-    if (document.getElementById("editor-preview") != null){
-      // textareaをMarkdownエディタにする
-      const easymdePreview = new EasyMDE({
-        showIcons: [],
-        autoDownloadFontAwesome: false,
-        toolbar: [{
-          name: "fullscreen",
-          action: EasyMDE.toggleFullScreen,
-          className: "fa fa-arrows-alt no-disable no-mobile",
-          title: "Toggle Fullscreen",
-        }],
-        status: false,
-        syncSideBySidePreviewScroll: false,
-        scrollbarStyle: null,
-        renderingConfig: {
-            codeSyntaxHighlighting: true,
-            hljs: hljs
-        },
-        element: document.getElementById("editor-preview"),
-      });
-      easymdePreview.togglePreview();
-    }
+    easymdePreview.togglePreview();
+  }
+}
+
+window.initMDE = initMDE;
+
+// 新規投稿ページ/投稿詳細ページのテキストエリアをマークダウンエディタへ変更
+'turbolinks:load'.split(' ').forEach((eventName)=>{
+  document.addEventListener(eventName, (e)=>{
+    initMDE();
   });
 });
