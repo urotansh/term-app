7474f9b50b2948ee57b93fd7d4ca26d65af11cc2
urotansh <114655455+urotansh@users.noreply.github.com>
2023-01-27 18:30:37 +0900

    [Update] コメント機能 コメント一覧画面のページネーションを非同期通信化

--- app/controllers/admin/note_comments_controller.rb
+++ app/controllers/admin/note_comments_controller.rb
@@ -4,6 +4,9 @@ class Admin::NoteCommentsController < ApplicationController
     if @note_comment.destroy
       # *.js.erbで参照するインスタンス
       @note = Note.find(params[:note_id])
+      @note_comments = @note.note_comments.order(updated_at: :desc)
+      # where/findなどを使用するとArrayオブジェクトになるため、Kaminariオブジェクトを呼び出す
+      @note_comments_pagination = Kaminari.paginate_array(@note_comments).page(params[:page]).per(5)
       render "destroy"
     else
       render "error"
--- app/controllers/public/note_comments_controller.rb
+++ app/controllers/public/note_comments_controller.rb
@@ -9,6 +9,9 @@ class Public::NoteCommentsController < ApplicationController
     if @note_comment.save
       # *.js.erbで参照するインスタンス
       @note = Note.find(params[:note_id])
+      @note_comments = @note.note_comments.order(updated_at: :desc)
+      # where/findなどを使用するとArrayオブジェクトになるため、Kaminariオブジェクトを呼び出す
+      @note_comments_pagination = Kaminari.paginate_array(@note_comments).page(params[:page]).per(5)
       render "create"
     else
       render "error"
@@ -21,6 +24,9 @@ class Public::NoteCommentsController < ApplicationController
     if @note_comment.destroy
       # *.js.erbで参照するインスタンス
       @note = Note.find(params[:note_id])
+      @note_comments = @note.note_comments.order(updated_at: :desc)
+      # where/findなどを使用するとArrayオブジェクトになるため、Kaminariオブジェクトを呼び出す
+      @note_comments_pagination = Kaminari.paginate_array(@note_comments).page(params[:page]).per(5)
       render "destroy"
     else
       render "error"
