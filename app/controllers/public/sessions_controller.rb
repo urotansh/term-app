3c6c93287619868c3af3d9137b8ade5f4a4aac86
urotansh <114655455+urotansh@users.noreply.github.com>
2023-02-24 16:03:33 +0900

    [Update] ゲストログイン機能 フラッシュメッセージの国際化(i18n)対応

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -9,7 +9,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(current_user.name), notice: "ゲストユーザでログインしました。"
+    redirect_to user_path(current_user.name), notice: I18n.t("user.notice.guest_sign_in")
   end
   
   # before_action :configure_sign_in_params, only: [:create]
