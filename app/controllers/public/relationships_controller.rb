1f3b0d54bc7b119c5384eeb888f712e003a72822
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-03 21:58:59 +0900

    [Improve] フォロー解除機能 パスパラメータ(ユーザー名)を変数へ格納

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,4 +1,7 @@
 class Public::RelationshipsController < ApplicationController
+  
+  before_action :set_path_param_name, only: [:destroy]
+  
   def create
     @user = User.find_by(name: params[:user_name])
     current_user.follow(@user)
@@ -8,4 +11,17 @@ class Public::RelationshipsController < ApplicationController
     @user = User.find_by(name: params[:user_name])
     current_user.unfollow(@user)
   end
+  
+  private
+  
+  def set_path_param_name
+    # パスパラメータ(ユーザー名)を変数へ格納
+    # Rails.application.routes.recognize_path(path, environment = {})
+    # => コントローラー, アクション, パスパラメータをハッシュで返却
+    # => {:controller=>"public/users", :action=>"show", :name=>"test1"}
+    # request.referrer
+    # => リクエスト元URL
+    @path_param_name = Rails.application.routes.recognize_path(request.referrer)[:name]
+  end
+  
 end
