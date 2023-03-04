0d0543c97b6a9f88e1b66863f898b8c72c3c6197
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-04 17:17:13 +0900

    [Improve] いいね/いいね解除機能 共通処理をメソッド化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,19 +1,21 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_note, only: [:create, :destroy]
   
   def create
     favorite = Favorite.new(user_id: current_user.id, note_id: params[:note_id])
     favorite.save
-    
-    # *.js.erbで参照するインスタンス
-    @note = Note.find(params[:note_id])
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id, note_id: params[:note_id])
     favorite.destroy
-    
-    # *.js.erbで参照するインスタンス
+  end
+  
+  private
+  
+  def set_note
     @note = Note.find(params[:note_id])
   end
+  
 end
