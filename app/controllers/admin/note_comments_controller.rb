5d6abefd12f78707a6558f583f4fd1dd08b87d18
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-05 20:11:09 +0900

    [Improve] コメント管理機能 共通処理をメソッド化

--- app/controllers/admin/note_comments_controller.rb
+++ app/controllers/admin/note_comments_controller.rb
@@ -1,9 +1,9 @@
 class Admin::NoteCommentsController < ApplicationController
+  before_action :set_note, only: [:destroy, :index]
+  
   def destroy
     @note_comment = NoteComment.find(params[:id])
     if @note_comment.destroy
-      # *.js.erbで参照するインスタンス
-      @note = Note.find(params[:note_id])
       @note_comments = @note.note_comments.order(updated_at: :desc)
       @note_comments_pagination = @note_comments.page(params[:page]).per(5)
       
@@ -18,12 +18,15 @@ class Admin::NoteCommentsController < ApplicationController
 
   def index
     # コメント一覧
-    @note = Note.find(params[:note_id])
     @note_comments = @note.note_comments.order(updated_at: :desc)
     @note_comments_pagination = @note_comments.page(params[:page]).per(5)
   end
   
   private
+  
+  def set_note
+    @note = Note.find(params[:note_id])
+  end
   
   def update_pager?(note_comments)
     # コメント件数が1ページへの最大表示件数で割り切れる場合、ページャを更新する
