d2deaadd05f3fd98238de456ce19ea1d048a16f3
urotansh <114655455+urotansh@users.noreply.github.com>
2023-03-31 22:40:18 +0900

    [Fix] コメント投稿機能 キーボードショートカットによるコメント投稿を有効化(ajax:completeによるイベントハンドリングができないケースの修正) fix #2

--- app/javascript/packs/ajax_submit.js
+++ app/javascript/packs/ajax_submit.js
@@ -1,17 +1,24 @@
 // import Rails from "@rails/ujs";
 
+// キーボードショートカット(Command + Enter or Ctrl + Enter)でコメントを投稿する
+const handleShortcutSubmit = function() {
+  const comment_form = document.getElementById('comment_form');
+  if (comment_form != null) {
+    comment_form.addEventListener('keydown', (e) => {
+      if (e.key == 'Enter' && (e.metaKey || e.ctrlKey) && !e.shiftKey) {
+        document.getElementById('comment_submit').click();
+        // rails-ujsを使用する場合
+        // WARNING: 2回目のコメント投稿でSQLite3::BusyException: database is lockedが発生してしまう
+        // Rails.fire(comment_form, 'submit');
+      }
+    });
+  }
+}
+
+window.handleShortcutSubmit = handleShortcutSubmit;
+
 'ajax:complete turbolinks:load'.split(' ').forEach((eventName)=>{
   document.addEventListener(eventName, (e)=>{
-    const comment_form = document.getElementById('comment_form');
-    if (comment_form != null) {
-      comment_form.addEventListener('keydown', (e) => {
-        if (e.key == 'Enter' && (e.metaKey || e.ctrlKey) && !e.shiftKey) {
-          document.getElementById('comment_submit').click();
-          // rails-ujsを使用する場合
-          // WARNING: 2回目のコメント投稿でSQLite3::BusyException: database is lockedが発生してしまう
-          // Rails.fire(comment_form, 'submit');
-        }
-      });
-    }
+    handleShortcutSubmit();
   });
 });
